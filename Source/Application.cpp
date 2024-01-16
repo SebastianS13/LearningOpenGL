f8e7917204bca4a9474a55da845a0f2f95bdd3f5
Sebastian Stachowicz <50264212+SebastianS13@users.noreply.github.com>
2024-01-16 18:57:07 +0000

    GLEW
    Implemented GLEW library

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -8,6 +11,8 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    std::cout << "Initiated GLFW" << std::endl;
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1250, 1000, "Learning OpenGL", NULL, NULL);
     if (!window)
@@ -16,8 +21,20 @@ int main(void)
         return -1;
     }
 
+    std::cout << "Created Window" << std::endl;
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    std::cout << "Set OpenGL window context" << std::endl;
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Glew is not in fact OK" << std::endl;
+    }
+
+    std::cout << "Initiated Glew" << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
